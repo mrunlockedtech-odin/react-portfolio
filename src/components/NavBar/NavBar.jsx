4176d0ece97101e9bdd0227e65c487878c7d0ece
mrunlockedtech <jrbhrn@gmail.com>
2022-10-15 17:40:15 -0500

    Create responsive menu for page navigation

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,28 +2,40 @@ import { Link } from "react-router-dom";
 
 
 const NavBar = () => {
-  return ( 
-    <nav>
-      <Link to='/about'>Naveen Bhatti, Jr.</Link>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/about'>About</Link>
-        </li>
-        <li>
-          <Link to='/contact'>Contact</Link>
-        </li>
-        <li>
-          <Link to='/resume'>Resume</Link>
-        </li>
-        <li>
-          <Link to='/projects'>Projects</Link>
-        </li>
-      </ul>
-    </nav>
-   );
+  return (
+    <div className="container">
+      <nav className="navbar navbar-expand-xl navbar-light">
+        <Link to='/about' className="navbar-brand">Naveen Bhatti, Jr.</Link>
+
+
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target='#toggleMobileMenu' aria-controls="toggleMobileMenu" aria-expanded='false' aria-label="Toggle Navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="toggleMobileMenu">
+
+        <ul className="navbar-nav ms-auto text-center">
+          <li>
+            <Link className="nav-link" to='/'>Home</Link>
+          </li>
+          <li>
+            <Link className="nav-link" to='/about'>About</Link>
+          </li>
+          <li>
+            <Link className="nav-link" to='/contact'>Contact</Link>
+          </li>
+          <li>
+            <Link className="nav-link" to='/resume'>Resume</Link>
+          </li>
+          <li>
+            <Link className="nav-link" to='/projects'>Projects</Link>
+          </li>
+        </ul>
+        </div>
+
+      </nav>
+    </div>
+
+  );
 }
- 
+
 export default NavBar;
