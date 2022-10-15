33e043a292ea02f19dea2f113c2d5234d091fbb9
mrunlockedtech <jrbhrn@gmail.com>
2022-10-15 11:14:30 -0500

    Load project information into data file

---
+++ src/data/projects.js
@@ -0,0 +1,16 @@
+const projects = [
+  {
+    title:"Service Ticket Tracker",
+    description:"Service Ticket Tracker is an app built to contain and interact with service desk tickets. The idea for this program was inspired by my past job as a Service Desk Support Specialist. The idea of managing tickets seemed to be the best application of my past working knowledge and my newly attained skills in CRUD.",
+    image:"https://github.com/mrunlockedtech-odin/service-ticket-tracker/blob/main/public/assets/images/homepage.png",
+    repositoryLink:"https://github.com/mrunlockedtech-odin/service-ticket-tracker",
+    deploymentLink:"https://service-ticket-tracker-ultra.fly.dev/"
+  },
+  {
+    title:"Connect 4",
+    description:"This project creates a connect-4 game that allows for two players to play. The game uses an array of solutions to compare user placements of their pieces to compute the winner of the game. The game has conditions for each player to win as well as a tie.",
+    image:"https://github.com/mrunlockedtech-odin/Connect-4-Naveen-Bhatti/blob/main/assets/images/gameSS2.png",
+    repositoryLink:"https://github.com/mrunlockedtech-odin/Connect-4-Naveen-Bhatti",
+    deploymentLink:"https://connect4naveen.netlify.app/"
+  },
+]
