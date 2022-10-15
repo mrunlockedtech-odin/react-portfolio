20365b5b0caaa8a853231169ae2990b77e34ebe8
mrunlockedtech <jrbhrn@gmail.com>
2022-10-15 11:08:48 -0500

    Add resume to portfolio

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,7 +1,10 @@
+import ResumeFile from '../../assets/NaveenBhatti-Resume.pdf'
+
 const Resume = () => {
   return ( 
     <>
     <h1>Resume</h1>
+    <p>Click the link to download <a href={ResumeFile}>my resume</a></p>
     </>
    );
 }
