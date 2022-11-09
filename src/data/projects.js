0954f12bb6a41e057bb621c3daf445ac530d2300
mrunlockedtech <jrbhrn@gmail.com>
2022-11-09 14:29:11 -0600

    Turn projects view into grid to account for additional objects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,22 +1,25 @@
+import Plane from '../assets/plane.png'
+import Serviceticket from '../assets/serviceticket.png'
+import Connect from '../assets/connect4.png'
 export const projects = [
   {
     title:"Service Ticket Tracker",
     description:"Service Ticket Tracker is an app built to contain and interact with service desk tickets. The idea for this program was inspired by my past job as a Service Desk Support Specialist. The idea of managing tickets seemed to be the best application of my past working knowledge and my newly attained skills in CRUD.",
-    image:"https://github.com/mrunlockedtech-odin/service-ticket-tracker/blob/main/public/assets/images/homepage.png?raw=true",
+    image:Serviceticket,
     repositoryLink:"https://github.com/mrunlockedtech-odin/service-ticket-tracker",
     deploymentLink:"https://service-ticket-tracker-ultra.fly.dev/"
   },
   {
     title:"Connect 4",
     description:"This project creates a connect-4 game that allows for two players to play. The game uses an array of solutions to compare user placements of their pieces to compute the winner of the game. The game has conditions for each player to win as well as a tie.",
-    image:"https://github.com/mrunlockedtech-odin/Connect-4-Naveen-Bhatti/blob/main/assets/images/gameSS2.png?raw=true",
+    image:Connect,
     repositoryLink:"https://github.com/mrunlockedtech-odin/Connect-4-Naveen-Bhatti",
     deploymentLink:"https://connect4naveen.netlify.app/"
   },
   {
     title:"Plan.e",
     description:"Plan.e is an app that allows users to create trip plans using crowd-sourced user activities. By making travelling a collaborative process, we want to make the experience for each of our users great.",
-    image:"https://github.com/thepika206/plane-front-end/blob/main/src/assets/PlaneLanding.png?raw=true",
+    image:Plane,
     repositoryLink:"https://github.com/thepika206/plane-front-end",
     deploymentLink:"https://wifi-travelers-plane.fly.dev/"
   },
