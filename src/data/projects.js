f58b2e3c3063473052363d832221517098483aa2
mrunlockedtech <jrbhrn@gmail.com>
2022-11-10 07:58:24 -0600

    Add Snack Collector Project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,26 +1,34 @@
 import Plane from '../assets/plane.png'
 import Serviceticket from '../assets/serviceticket.png'
 import Connect from '../assets/connect4.png'
+import Snack from '../assets/snackcollector.png'
 export const projects = [
   {
-    title:"Service Ticket Tracker",
-    description:"Service Ticket Tracker is an app built to contain and interact with service desk tickets. The idea for this program was inspired by my past job as a Service Desk Support Specialist. The idea of managing tickets seemed to be the best application of my past working knowledge and my newly attained skills in CRUD.",
-    image:Serviceticket,
-    repositoryLink:"https://github.com/mrunlockedtech-odin/service-ticket-tracker",
-    deploymentLink:"https://service-ticket-tracker-ultra.fly.dev/"
-  },
-  {
     title:"Connect 4",
-    description:"This project creates a connect-4 game that allows for two players to play. The game uses an array of solutions to compare user placements of their pieces to compute the winner of the game. The game has conditions for each player to win as well as a tie.",
+    description:"This project creates a connect-4 game that allows for two players to play. The game uses an array of solutions to compare user placements of their pieces to compute the winner of the game. The game has conditions for each player to win as well as a tie. Concepts: HTML, CSS, Javascript",
     image:Connect,
     repositoryLink:"https://github.com/mrunlockedtech-odin/Connect-4-Naveen-Bhatti",
     deploymentLink:"https://connect4naveen.netlify.app/"
   },
   {
+    title:"Service Ticket Tracker",
+    description:"Service Ticket Tracker is an app built to contain and interact with service desk tickets. The idea for this program was inspired by my past job as a Service Desk Support Specialist. Concepts: HTML, CSS, Javascript, Node.js, Express.js, CRUD",
+    image:Serviceticket,
+    repositoryLink:"https://github.com/mrunlockedtech-odin/service-ticket-tracker",
+    deploymentLink:"https://service-ticket-tracker-ultra.fly.dev/"
+  },
+  {
     title:"Plan.e",
-    description:"Plan.e is an app that allows users to create trip plans using crowd-sourced user activities. By making travelling a collaborative process, we want to make the experience for each of our users great.",
+    description:"Plan.e is an app that allows users to create trip plans using crowd-sourced user activities. By making travelling a collaborative process, we want to make the experience for each of our users great. Technologies: React, Full-Stack, CSS, JS, HTML",
     image:Plane,
     repositoryLink:"https://github.com/thepika206/plane-front-end",
+    deploymentLink:"https://wifi-travelers-plane.netlify.app/"
+  },
+  {
+    title:"Snack-Collector",
+    description:"Snack-Collector is a simple app that allows users to keep track of their snacks, the date they purchased them on, and dishes that may include them. Technologies: Django, CSS, Python, HTML, Javascript",
+    image:Snack,
+    repositoryLink:"https://github.com/mrunlockedtech-odin/snack-collector",
     deploymentLink:"https://wifi-travelers-plane.fly.dev/"
   },
 
--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,10 +1,12 @@
 import ResumeFile from '../../assets/NaveenBhatti-Resume.pdf'
+import Document from '../../assets/document.svg'
 
 const Resume = (props) => {
   return ( 
     <div ref={props.resumeRef} id="resumeProfile">
-    <h1>Resume</h1>
-    <p>Click the link to download <a href={ResumeFile}>my resume</a></p>
+      <img src={Document} alt="document icon" />
+      <h1>Resume</h1>
+      <p>Click the link to download <a href={ResumeFile}>my resume</a></p>
     </div>
    );
 }
